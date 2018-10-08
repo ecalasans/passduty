a7cee95eae0823689794c5c24473d7bb37c2053d
ecalasans <e1906d0103>
2018-10-08 16:38:14 -0300

    Modelos terminados

--- passagem/models.py
+++ passagem/models.py
@@ -19,6 +19,7 @@ class Paciente(models.Model):
     nome = models.CharField(max_length=250)
     leito = models.ForeignKey(Leito, on_delete=models.DO_NOTHING)
     pesoNasc = models.IntegerField()
+    dataNasc = models.DateField()
 
     def __str__(self):
         return self.nome
@@ -33,3 +34,26 @@ class Medico(models.Model):
         return self.nome
 
 
+class HospMed(models.Model):
+    hospId = models.ForeignKey(Hospital, on_delete=models.CASCADE)
+    medId = models.ForeignKey(Medico, on_delete=models.CASCADE)
+
+
+class Anotacao(models.Model):
+    medId = models.ForeignKey(Medico, on_delete=models.CASCADE)
+    pacId = models.ForeignKey(Paciente, on_delete=models.DO_NOTHING)
+    dataHora = models.DateTimeField()
+    idade = models.SmallIntegerField()
+    igc = models.CharField(max_length=20)
+    pesoAtual = models.IntegerField()
+    diagn = models.CharField(max_length=200)
+    dieta = models.CharField(max_length=100)
+    acesso = models.CharField(max_length=2, default='N') #Nenhum, Periférico, Punção, Dissecção, PIcc
+    atbMed = models.CharField(max_length=100)
+    ventilacao = models.CharField(max_length=2, default='A') #Ar ambiente, CPAP, VNI, VM
+    fototerapia = models.BooleanField(default=False)
+    exames = models.CharField(default=200)
+    conduta = models.CharField(default=500)
+
+    class Meta:
+        ordering = ('-dataHora', )
